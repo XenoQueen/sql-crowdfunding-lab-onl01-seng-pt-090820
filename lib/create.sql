89c6a387536491626d027e34d4e0f16ca69afd04
Maika Kailani <queenmab051@gmail.com>
2020-11-10 17:06:41 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,7 +4,3 @@ CREATE TABLE groceries (
   quantity INTEGER,
   aisle INTEGER
   );
-  
-  INSERT INTO groceries VALUES (1, "Apples", 2, 1);
-  INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
-  INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
